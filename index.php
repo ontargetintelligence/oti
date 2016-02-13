4f5087004a3e999ff084da6d226ff0f8128c4902
i7nvd <therealpaulgrant@gmail.com>
2016-02-13 15:23:39 -0500

    Update to latest version

---
+++ index.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no">
+        <meta name="description" content="Responsive HTML5 Single Page Design Template">
+        <meta name="keywords" content="Bootstrap, Bootstrap 3, Font Awesome, Ion Icons, Ionicons, HTML5, CSS, CSS3, JavaScript, Template, JS, jQuery, Ajax, work, single-page, one-page, Theme, Twitter, Facebook">
+
+        <title>On-Target Intelligence</title>
+
+        <link href="favicon1.ico" rel="icon" type="image/x-icon" />
+
+        <!-- PRE-LOADER JAVASCRIPT START -->
+        <script src="js/pace.min.js" type="text/javascript"></script>
+        <!-- PRE-LOADER JAVASCRIPT END -->
+
+        <!-- STYLESHEETS START -->
+        <!-- Bootstrap style sheet -->
+        <link rel="stylesheet" href="css/bootstrap.css" />
+        <!-- Font Awesome style sheet -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <!-- Ion Icons style sheet -->
+        <link rel="stylesheet" href="css/ionicons.min.css">
+        <!-- Pretty photo style sheet -->
+        <link rel="stylesheet" href="css/prettyPhoto.css">
+        <!-- Sunshine specific style sheet -->
+        <link rel="stylesheet" href="css/styles.css" />
+        <!-- STYLESHEETS END -->
+
+        <!-- FONTS START -->
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet" type="text/css">
+        <!-- FONTS END -->
+    </head>
+    <body data-spy="scroll" data-target="#top-navigation">
+        <!-- PAGE START -->
+        <div id="page">
+            <!-- MAIN NAV START -->
+            <nav id="top-navigation" class="navbar navbar-default navbar-transparent navbar-fixed-top" role="navigation">
+                <div class="navbar-header">
+                    <button id="menu-mobile" type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
+                        <span class="fa fa-bars"></span>
+                    </button>
+                    <!-- LOGO COMES HERE -->
+                    <a class="navbar-brand scroll" href="#header">On-Target Intelligence</a>
+                </div>
+
+                <!-- NAV ITEMS START -->
+                <div class="collapse navbar-collapse" id="navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#header" class="scroll">Home</a></li>
+                        <li><a href="#about" class="scroll">About Us</a></li>
+                        <li><a href="#services" class="scroll">Services</a></li>
+                        <li><a href="#clients" class="scroll">Testimonials</a></li>
+                        <li><a href="#managingdirector" class="scroll">Managing Director</a></li>
+                        <li><a href="#contact" class="scroll">Contact</a></li>
+                    </ul>
+                </div>
+                <!-- NAV ITEMS END -->
+            </nav>
+            <!-- MAIN NAV END -->
+            <!-- HOME START -->
+            <header id="header">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-10 col-xs-offset-1 col-sm-5" id="intro">
+                            <h1><strong>On-Target</strong> Intelligence</h1>
+                            <br>
+                            <p>We assist our clients in improving their bottom lines by enhancing their intelligence collection and processing capabilities.</p>
+                            <br>
+                            <a href="#about" class="btn btn-transparent scroll"><strong>LEARN MORE</strong></a>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <!-- HOME END -->
+
+            <!-- ABOUT START -->
+            <section id="about">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <p class="fancy"><span>About Us</span></p>
+                        </div>
+                        <div class="article col-xs-12 col-sm-6">
+                            <i class="ion-compass fa-2x otblue"></i>
+                            <h3>Philosophy</h3>
+                            <p>Intelligence is not about the information collected, but rather the positive impact it has on targeted business processes and outcomes.</p>
+                        </div>
+                        <div class="article col-xs-12 col-sm-6">
+                            <i class="ion-pinpoint fa-2x otblue"></i>
+                            <h3>Motivation</h3>
+                            <p>To assist our clients in establishing and enhancing their intelligence methodologies, organizations, and tools to ensure they have the greatest likelihood of success, what ever their business endeavours.</p>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+            <!-- ABOUT END -->
+
+            <!-- SERVICES START -->
+            <section id="services">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <p class="fancy"><span>Services</span></p>
+                        </div>
+                        <div class="desc col-xs-12 col-sm-6 col-sm-offset-3">
+                            <p>We offer our clients support from understanding and implementing the basics of intelligence through to the complex integration of intelligence support and targeted business operations.  Our products and services include:</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="article col-xs-12 col-sm-3">
+                            <i class="ion-alert fa-2x otblue"></i>
+                            <h3>Intelligence</h3>
+                            <p>Advice on new and effective intelligence planning, analysis, and dissemination capabilities and processes, and the efficient and effective integration of intelligence collection capabilities and methods to maximize the probability of successful intelligence operations.</p>
+                        </div>
+                        <div class="article col-xs-12 col-sm-3">
+                            <i class="ion-ios7-people fa-2x otblue"></i>
+                            <h3>Training</h3>
+                            <p>Training support for both intelligence and targeting, including education and exercises.</p>
+                        </div>
+                        <div class="article col-xs-12 col-sm-3">
+                            <i class="ion-clipboard fa-2x otblue"></i>
+                            <h3>Doctrine</h3>
+                            <p>Capturing business principles, tenets, and processes in a clear and articulate format.</p>
+                        </div>
+                        <div class="article col-xs-12 col-sm-3">
+                            <i class="ion-eye fa-2x otblue"></i>
+                            <h3>Targeting</h3>
+                            <p>Assistance and advice on the full range of targeting issues from, selection of desired outcomes, operational guidance, target selection and development, planning and review processes.</p>
+                        </div>						
+                    </div>
+                </div>
+            </section>
+            <!-- SERVICE END -->
+
+ 
+            <!-- CLIENTS START -->
+            <section id="clients">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <p class="fancy"><span>Testimonials</span></p>
+                        </div>
+                        <div class="desc col-xs-12 col-sm-6 col-sm-offset-3 intro">
+                            <p>Here's what On-Target Intelligence clients have said about the services provided:</p>
+                        </div>
+                    </div>
+                    <div class="row statement">
+                        <div class="col-xs-6 col-md-6">
+                            <h4><strong>Canadian Forces Warfare Centre</strong></h4>
+                            <p>"In support of the development of the scenario for JOINTEX 13, Mr Grant brought expertise, detailed analysis and exacting product development skills to the scenario development team.  The targeting related products and injects Mr Grant provided ensured realism and provided the opportunity for doctrinally correct, in depth analysis for exercise participants.  Mr Grant is a true intelligence professional who I would gladly welcome on my team again for future exercise development."</p>
+                        </div>
+                        <div class="col-xs-6 col-md-6">
+                            <h4><strong>CALIAN Ltd.</strong></h4>
+                            <p>"We have employed Pat Grant in a number of challenging and diverse positions over the past year.  He can always be counted on to provided accurate, timely and well thought out products in which his attention to detail and personal initiative have come to the fore.  Pat is confident, articulate and is able to provide clarity to difficult subject matter through verbal or written dissemination."</p>
+                    </div>
+                </div>
+            </section>
+            <!-- CLIENTS END -->
+
+            <!-- MANAGING DIRECTOR START -->
+            <section id="managingdirector">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <p class="fancy"><span>Managing Director</span></p>
+                        </div>
+                        <div class="desc col-xs-12 col-sm-6 col-sm-offset-3 intro">
+                            <h2>Patrick Grant</h2>
+                            <p>Patrick Grant is a retired Canadian Force senior Intelligence Officer.  He has 23 years experience in the intelligence field, mostly in direct support of the planning and the conduct of operations, at home and internationally.  Pat also coordinated the development of new capabilities, from research and development to implementation, and for the codification of joint army, navy and air force operational principles and processes.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- MANAGING DIRECTOR END -->
+
+            <!-- CONTACT START -->
+            <section id="contact">
+                <div class="container" id="contact-details-wrapper">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <p class="fancy"><span>Contact</span></p>
+                        </div>
+                    </div>
+                    <div id="contact-details">
+                        <div class="row">
+                            <div class="desc col-xs-12 col-sm-6 col-sm-offset-3 intro">
+                                <h2>Have a question?</h2>
+                                <p>Drop us a line. We'd be happy to assess your needs.</p>
+                            </div>
+                        </div>
+                        <!-- CONTACT HAS 2 OPTIONS: Form based (option 1) and Address/Phone/Social (option 2) -->
+                        <div class="row">
+                            <!-- CONTACT OPTION 1 START -->
+                            <div class="option col-xs-10 col-xs-offset-1 col-sm-5 col-sm-offset-1">
+                                <!-- CONTACT FORM START -->
+                                <form role="form" id="contact-form">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="name" id="name" placeholder="Name">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="email" class="form-control" name="email" id="email" placeholder="Email">
+                                    </div>
+                                    <div class="form-group">
+                                        <textarea class="form-control" rows="8" name="message" id="message" placeholder="Message"></textarea>
+                                    </div>
+                                    <div class="form-group">
+                                        <a class="btn btn-otblue btn-half-block" id="submit-contact-form">Send</a>
+                                    </div>
+                                </form>
+                                <!-- CONTACT FORM END -->
+                                <div class="alert alert-success alert-small" id="contact-success" style="display: none;"><i class="ion-checkmark-round"></i> <span>Message sent.</span></div>
+                                <div class="alert alert-danger alert-small" id="contact-error" style="display: none;"><i class="ion-close-round"></i> <span>Something is not working. Please try again.</span></div>
+                            </div>
+                            <!-- CONTACT OPTION 1 END -->
+                            <!-- CONTACT OPTION 2 START -->
+                            <div class="option col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-1">
+                                 <div class="media">
+                                    <i class="pull-left ion-ios7-telephone-outline fa-2x text-muted"></i>
+                                    <div class="media-body">
+                                        (613) 371-2747
+                                    </div>
+                                </div>
+                                <div class="media">
+                                    <i class="pull-left ion-ios7-email-outline fa-2x text-muted"></i>
+                                    <div class="media-body">
+                                        <a href="mailto:pat.grant@ontargetintelligence.ca">pat.grant@ontargetintelligence.ca</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- CONTACT OPTION 2 END -->
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- CONTACT END -->
+
+            <!-- FOOTER START -->
+            <footer id="footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <p class="text-muted">&copy; Copyright <?php echo date("Y"); ?> On-Target Intelligence Inc. All rights reserved. Website created by <a href="http://www.tinyglowstudio.com">Tiny Glow Studio</a>.</p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <!-- FOOTER END -->
+        </div>
+        <!-- PAGE END -->
+
+        <!-- JAVASCRIPT START -->
+        <!-- jQuery script -->
+        <script src="js/jquery-1.10.2.min.js" type="text/javascript"></script>
+        <!-- Bootstrap script -->
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <!-- Pretty photo script -->
+        <script src="js/jquery.prettyPhoto.js" type="text/javascript"></script>
+        <!-- Parallax script -->
+        <script src="js/jquery.parallax-1.1.3.js" type="text/javascript"></script>
+        <!-- Mixitup Filters script -->
+        <script src="js/jquery.mixitup.min.js" type="text/javascript"></script>
+
+        <!-- Sunshine specific Scripts -->
+        <script src="js/scripts.js" type="text/javascript"></script>
+        <!-- JAVASCRIPT END -->
+            <script type="text/javascript">
+            $(document).ready(function(){  
+                //.parallax(xPosition, speedFactor, outerHeight) options:
+                //xPosition - Horizontal position of the element
+                //inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
+                //outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
+                $('#header').parallax("right", 0.85);
+				$('#managingdirector').parallax("50%", 0.1);
+				$('#clients').parallax("50%", 2);
+            })
+        </script>
+    </body>
+</html>
--- send_message.php
+++ send_message.php
@@ -1,16 +1,75 @@
 <?php
+
+require_once('phpmailer/PHPMailerAutoload.php');
+
 $name       = "";
-$email      = "";
+$email    	= "";
 $message    = "";
 $newsletter = 0;
 
-//Set the variables based on GET parameters
+//Set the variables based on POST parameters
 foreach ($_POST as $key => $value) {
     if(isset($_POST[$key])) {
         ${$key} = $value;
     }
 }
 
-echo '{"name": "'.$name.'", "email": "'.$email.'", "message": "'.$message.'", "newsletter": "'.$newsletter.'"}';
+$data = '{"name": "'.$name.'", "email": "'.$email.'", "message": "'.$message.'"}';
 
-?>
+//Create a new PHPMailer instance
+$mail = new PHPMailer();
+
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 0;
+
+//Ask for HTML-friendly debug output
+$mail->Debugoutput = 'html';
+
+//Set the hostname of the mail server
+$mail->Host = 'p3plcpnl0086.prod.phx3.secureserver.net';
+
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 465;
+
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'ssl';
+
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "pat.grant@ontargetintelligence.ca";
+
+//Password to use for SMTP authentication
+$mail->Password = "?IcmT=P&wR4.";
+
+//Set who the message is to be sent from
+$mail->setFrom('pat.grant@ontargetintelligence.ca', 'Website Contact Form');
+
+//Set an alternative reply-to address
+$mail->addReplyTo('pat.grant@ontargetintelligence.ca', 'Do Not Reply');
+
+//Set who the message is to be sent to
+$mail->addAddress('pat.grant@ontargetintelligence.ca', 'Patrick Grant');
+
+//Set the subject line
+$mail->Subject = 'Mail from website contact form';
+
+//Set the mail body
+$mail->Body = $data;
+
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+    header("Status: 404");
+} else {
+    echo "Message sent!";
+}
+
+?> 
